c4bc3ad1ecf51deda2359559ce77b3f6d5113235
Iamsreerag <89203874+Iamsreerag@users.noreply.github.com>
2022-02-04 15:54:41 +0530

    Update sreerag.py

--- sreerag.py
+++ sreerag.py
@@ -22,8 +22,8 @@ SREERAG = [
 @sreerag.on_message(filters.command("start")) 
 async def start_message(bot, message):
     await message.reply_photo(
-        photo=random.choice(SREERAG)
-        caption="Enter ur text here", 
+        photo=random.choice(SREERAG), 
+        caption="hy", 
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("[🅜🅒]New Movies", url="https://t.me/joinchat/slPWoPDfoJc3NTVl"), 
             InlineKeyboardButton("[🅜🅒]GROUP", url="https://t.me/malayayalies")
