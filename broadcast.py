1d275c06dce7858ad60874909a286c2b4bfc06b0
Iamsreerag <89203874+Iamsreerag@users.noreply.github.com>
2022-03-25 17:23:03 +0530

    Update broadcast.py

--- broadcast.py
+++ broadcast.py
@@ -3,7 +3,7 @@ from pyrogram import Client ,filters
 from helper.database import getid
 ADMIN = int(os.environ.get("ADMIN", 923943045))
 
-@Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
+@sreerag.on_message(filters.private & filters.user(ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
  if (message.reply_to_message):
    ms = await message.reply_text("Geting All ids from database ...........")
