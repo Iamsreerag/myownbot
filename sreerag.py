d76cdbde15745b8a1db18b6a1d2143259db56436
Iamsreerag <89203874+Iamsreerag@users.noreply.github.com>
2022-03-26 22:34:25 +0530

    Update sreerag.py

--- sreerag.py
+++ sreerag.py
@@ -1,8 +1,7 @@
 from pyrogram import Client,filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random
-import pymongo
-from userbase import present_in_userbase
+from userbase import present_in_userbase, add_to_userbase
 sreerag=Client(
     "myownbot", 
     bot_token="5158569357:AAEvZRy0t9h6OotO2n6o-ksZV6CeNEp9Ujo", 
